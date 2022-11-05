3fc2eb5afe49a31d3cb0c02bad4975335c0c6bd0
luunghia187 <94558974+luunghia187@users.noreply.github.com>
2022-11-06 01:37:58 +0700

    connect data remotemysql

--- src/common/connect.js
+++ src/common/connect.js
@@ -1,14 +1,22 @@
 var mysql = require('mysql');
 
+// var connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'pbl6'
+// });
+
 var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'pbl6'
+    host: 'remotemysql.com',
+    user: 'FpOP5swXpw',
+    password: 'vaYhu1dWAL',
+    database: 'FpOP5swXpw'
 });
 
 connection.connect(function (err, connection) {
     if(err) console.log("ket noi that bai");
+    else console.log("ket noi");
 });
 
 module.exports = connection;
